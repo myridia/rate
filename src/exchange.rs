c0093913f392b7bfce8e66131561d511dfcd58ba
veto <veto@dev.local>
2026-01-05 11:44:23 +0700

    up: Cargo.lock Cargo.toml src/exchange.rs

--- src/exchange.rs
+++ src/exchange.rs
@@ -1,11 +1,40 @@
 use axum::{Json, extract, extract::Query, response::IntoResponse};
+use chrono::Utc;
 use reqwest::*;
 use rusqlite::{Connection, Result};
 use serde::{Deserialize, Serialize};
+use serde_xml_rs::from_str;
 use std::collections::HashMap;
+
 use std::error::Error;
 
-use chrono::Utc;
+#[allow(non_snake_case)]
+#[derive(Debug, Deserialize)]
+struct Envelope {
+    #[serde(rename = "Cube")]
+    pub cube_root: CubeRoot,
+}
+
+#[derive(Debug, Deserialize)]
+struct CubeRoot {
+    // This targets the <Cube time="..."> level
+    #[serde(rename = "Cube")]
+    pub time_entries: Vec<TimeEntry>,
+}
+
+#[derive(Debug, Deserialize)]
+struct TimeEntry {
+    pub time: String,
+    // This targets the <Cube currency="..." rate="..."> level
+    #[serde(rename = "Cube")]
+    pub rates: Vec<RateEntry>,
+}
+
+#[derive(Debug, Deserialize)]
+struct RateEntry {
+    pub currency: String,
+    pub rate: f64,
+}
 
 pub async fn ecb() -> Result<String, Box<dyn Error>> {
     let url = format!("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
@@ -16,8 +45,26 @@ pub async fn ecb() -> Result<String, Box<dyn Error>> {
         .send()
         .await;
     let res = _res.unwrap().text().await.unwrap().to_string();
-    //let inv: Invoice = serde_json::from_str(&r).unwrap();
-    println!("{:?}", res);
+
+    //let p: Envelope = from_str(&res).unwrap();
+    //println!("{:?}", p);
+    let mut rates: HashMap<String, f64> = HashMap::new();
+    let mut date = "".to_string();
+    match from_str::<Envelope>(&res) {
+        Ok(envelope) => {
+            for time_entry in envelope.cube_root.time_entries {
+                //println!("Date: {}", time_entry.time);
+                date = format!("{0}16", time_entry.time.to_string().replace("-", ""));
+                for r in time_entry.rates {
+                    //println!("  {} : {}", r.currency, r.rate);
+                    rates.insert(r.currency, r.rate);
+                }
+            }
+        }
+        Err(e) => println!("Error: {}", e),
+    }
+    println!("{:?}", rates);
+    println!("{:?}", date);
     /*
     let ret = client.get(url)
         .await
